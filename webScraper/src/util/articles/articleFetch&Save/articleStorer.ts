0fa2e0a9b26acacc37636d735b2c05cc5f33d237
Mabon ap Gwyn <mabonapgwyn@hotmail.co.uk>
2022-03-19 19:31:38 +0000

    empty articles removed

--- webScraper/src/util/articles/articleFetch&Save/articleStorer.ts
+++ webScraper/src/util/articles/articleFetch&Save/articleStorer.ts
@@ -1,14 +1,16 @@
 import fs from 'fs';
 import ArticleModel from '../../../model/articleModel';
-import {quoteFormatter, htmlTagStrip} from '../articleFormatting/articleTextFormatter';
+import { quoteFormatter, htmlTagStrip } from '../articleFormatting/articleTextFormatter';
 
 export default function articleStorer(articlesArray: ArticleModel[]) {
+    const finalArticleArray = [];
     articlesArray.forEach(article => {
         if (article.articleText) {
             article.articleText = _formatArticleText(article.articleText);
+            finalArticleArray.push(article)
         }
     })
-    _createJSONEntry(articlesArray)
+    _createJSONEntry(finalArticleArray);
 }
 
 
