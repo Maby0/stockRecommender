a115aa4e318033369583fdc38ef441ee4cd2733a
Mabon ap Gwyn <mabonapgwyn@hotmail.co.uk>
2022-03-25 11:38:50 +0000

    remove unnecessary constants folder

--- webScraper/src/constants/constants.ts
+++
@@ -1,5 +0,0 @@
-// import loadArticles from "../util/articles/articleLoad/loadArticlesToMemory";
-// import loadSNPData from "../util/SNP500/SNP500DataLoad/loadSNPDataToMemory";
-
-// export const allStoredArticles = loadArticles();
-// export const SNP500CompaniesArray = loadSNPData();
